7e06a6e3acc00710dc3a832b1a9048d0653d1a3c
Nicfrey <nicolas.fer9@gmail.com>
2024-06-08 19:19:06 +0200

    fix: unused parameter

--- Minigin/NullSoundSystem.h
+++ Minigin/NullSoundSystem.h
@@ -1,21 +1,37 @@
 #pragma once
 #include "SoundSystemEngine.h"
 
-class NullSoundSystem : public SoundSystem
+class NullSoundSystem final : public SoundSystem
 {
 public:
 	~NullSoundSystem() override = default;
-	void Play(const SoundId& id, const float& volume) override {}
-	void Add(const SoundId& id, const std::string& filepath) override {}
+	void Play(const SoundId& id, const float& volume) override
+	{
+		(void)id;
+		(void)volume;
+	}
+	void Add(const SoundId& id, const std::string& filepath) override
+	{
+		(void)id;
+		(void)filepath;
+	}
 	void Mute() override {}
 };
 
-class NullMusicSystem : public MusicSystem
+class NullMusicSystem final : public MusicSystem
 {
 public:
 	~NullMusicSystem() override = default;
-	void Play(MusicId id, bool loop) override {}
-	void Add(MusicId id, const std::string& filePath) override {}
+	void Play(MusicId id, bool loop) override
+	{
+		(void)id;
+		(void)loop;
+	}
+	void Add(MusicId id, const std::string& filePath) override
+	{
+		(void)id;
+		(void)filePath;
+	}
 	void Pause() override {}
 	void Resume() override {}
 	void Stop() override {}
